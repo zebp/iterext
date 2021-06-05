8af96d145ece0e172c490b70451d4b07733d5011
Zeb Piasecki <zeb@zebulon.dev>
2021-06-05 13:56:24 -0400

    wip: begin implementing AsyncIter

---
+++ async.test.ts
@@ -0,0 +1,140 @@
+import {
+  assertEquals,
+  assertMatch,
+} from "https://deno.land/std@0.97.0/testing/asserts.ts";
+import { AsyncIter } from "./async.ts";
+
+async function* sequentialIntegers(start: number = 1, end: number = 10) {
+  for (let i = start; i <= end; i++) {
+    yield i;
+  }
+}
+
+Deno.test({
+  name: "create async iter",
+  fn() {
+    new AsyncIter(sequentialIntegers());
+  },
+});
+
+Deno.test({
+  name: "is valid iterator",
+  async fn() {
+    assertEquals(await new AsyncIter(sequentialIntegers()).next(), {
+      done: false,
+      value: 1,
+    });
+  },
+});
+
+Deno.test({
+  name: "is valid iterable",
+  async fn() {
+    for await (const item of new AsyncIter(sequentialIntegers())) {
+      assertEquals(item, 1);
+      break;
+    }
+  },
+});
+
+Deno.test({
+  name: "collect iter",
+  async fn() {
+    const items = await new AsyncIter(sequentialIntegers()).collect();
+
+    assertEquals(items, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+  },
+});
+
+Deno.test({
+  name: "reduce iter",
+  async fn() {
+    const tenFactorial = await new AsyncIter(sequentialIntegers(1, 10)).reduce(
+      (prev, item) => prev * item,
+      1,
+    );
+
+    assertEquals(tenFactorial, 3628800);
+  },
+});
+
+Deno.test({
+  name: "filter iter",
+  async fn() {
+    const evenNumbers = await new AsyncIter(sequentialIntegers(1, 10)).filter(
+      async (
+        x,
+      ) => x % 2 == 0,
+    ).collect();
+
+    assertEquals(evenNumbers, [2, 4, 6, 8, 10]);
+  },
+});
+
+Deno.test({
+  name: "filterSync iter",
+  async fn() {
+    const evenNumbers = await new AsyncIter(sequentialIntegers(1, 10))
+      .filterSync((
+        x,
+      ) => x % 2 == 0).collect();
+
+    assertEquals(evenNumbers, [2, 4, 6, 8, 10]);
+  },
+});
+
+Deno.test({
+  name: "map iter",
+  async fn() {
+    const result = await new AsyncIter(sequentialIntegers(1, 10)).map(
+      async (
+        x,
+      ) => x * 2,
+    ).collect();
+
+    assertEquals(result, [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]);
+  },
+});
+
+Deno.test({
+  name: "mapSync iter",
+  async fn() {
+    const result = await new AsyncIter(sequentialIntegers(1, 10)).mapSync((
+      x,
+    ) => x * 2).collect();
+
+    assertEquals(result, [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]);
+  },
+});
+
+Deno.test({
+  name: "filterMap iter",
+  async fn() {
+    const result = await new AsyncIter(sequentialIntegers(1, 10))
+      .filterMap(
+        async (
+          x,
+        ) => {
+          return x % 2 == 0 ? undefined : x * 2;
+        },
+      ).collect();
+
+    assertEquals(result, [2, 6, 10, 14, 18]);
+  },
+});
+
+Deno.test({
+  name: "filterMapSync iter",
+  async fn() {
+    const result = await new AsyncIter(sequentialIntegers(1, 10))
+      .filterMapSync(
+        (
+          x,
+        ) => {
+          return x % 2 == 0 ? undefined : x * 2;
+        },
+      ).collect();
+
+    assertEquals(result, [2, 6, 10, 14, 18]);
+  },
+});
---
+++ async.ts
@@ -0,0 +1,155 @@
+/**
+ * A wrapper of an {@link AsyncIterator} that implements many useful methods.
+ * Modelled after {@link https://docs.rs/futures/0.3.15/futures/stream/trait.StreamExt.html}.
+ */
+export class AsyncIter<T> implements AsyncIterator<T>, AsyncIterable<T> {
+  #inner: AsyncIterator<T>;
+
+  constructor(inner: AsyncIterator<T>) {
+    this.#inner = inner;
+  }
+
+  /**
+   * Filters elements that pass through the iterator.
+   * @param predicate if the item should be included in the resulting iterator.
+   * @returns a {@link AsyncIter} where all elements have passed the predicate.
+   */
+  filter(predicate: (item: T) => Promise<boolean>): AsyncIter<T> {
+    const iter = this;
+
+    return new AsyncIter(async function* () {
+      for await (const item of iter) {
+        if (await predicate(item)) yield item;
+      }
+    }());
+  }
+
+  /**
+   * Filters elements that pass through the iterator.
+   * @param predicate if the item should be included in the resulting iterator.
+   * @returns a {@link AsyncIter} where all elements have passed the predicate.
+   */
+  filterSync(predicate: (item: T) => boolean): AsyncIter<T> {
+    const iter = this;
+
+    return new AsyncIter(async function* () {
+      for await (const item of iter) {
+        if (predicate(item)) yield item;
+      }
+    }());
+  }
+
+  /**
+   * Processes all items with the provided function and yields all outputs that were defined
+   * @param func the function to map an item that returns an output if it should be yielded or
+   * undefined if it shouldn't.
+   * @returns a {@link AsyncIter} where all elements have been mapped and filtered through the
+   * function.
+   */
+  filterMap<Output>(
+    func: (item: T) => Promise<Output | undefined>,
+  ): AsyncIter<Output> {
+    const iter = this;
+
+    return new AsyncIter(async function* () {
+      for await (const item of iter) {
+        const mapped = await func(item);
+        if (mapped !== undefined) yield mapped;
+      }
+    }());
+  }
+
+  /**
+   * Processes all items with the provided function and yields all outputs that were defined
+   * @param func the function to map an item that returns an output if it should be yielded or
+   * undefined if it shouldn't.
+   * @returns a {@link AsyncIter} where all elements have been mapped and filtered through the
+   * function.
+   */
+  filterMapSync<Output>(
+    func: (item: T) => Output | undefined,
+  ): AsyncIter<Output> {
+    const iter = this;
+
+    return new AsyncIter(async function* () {
+      for await (const item of iter) {
+        const mapped = func(item);
+        if (mapped !== undefined) yield mapped;
+      }
+    }());
+  }
+
+  /**
+   * Asynchronously maps the items in the iterator with the provided function and then awaits their
+   * result. If you would not like to await the resulting promise, use {@method mapSync}.
+   * @param func a function to map items.
+   * @returns a {@link AsyncIter} where all elements have been mapped by the provided function.
+   */
+  map<Output>(
+    func: (item: T) => Promise<Output>,
+  ): AsyncIter<Output> {
+    const iter = this;
+
+    return new AsyncIter(async function* () {
+      for await (const item of iter) {
+        yield await func(item);
+      }
+    }());
+  }
+
+  /**
+   * Synchronously maps the items in the iterator with the provided function. If you would like to
+   * await the result and your function returns a {@link Promise}, use {@method map}.
+   * @param func a function to map items.
+   * @returns a {@link AsyncIter} where all elements have been mapped by the provided function.
+   */
+  mapSync<Output>(
+    func: (item: T) => Output,
+  ): AsyncIter<Output> {
+    const iter = this;
+
+    return new AsyncIter(async function* () {
+      for await (const item of iter) {
+        yield func(item);
+      }
+    }());
+  }
+
+  /**
+   * Iterates through the entire iterator and collects the items as an array.
+   * @returns all the items in the iterator as an array.
+   */
+  collect(): Promise<T[]> {
+    return this.reduce<T[]>((prev, curr) => [...prev, curr], []);
+  }
+
+  /**
+   * Iterates through the entire iterator reducing all items to one value.
+   * @param func a function executed on each item of the iterator with the previous value supplied.
+   * @param initial the initial value to be passed to the reducer function.
+   * @returns the iterator reduced down to a single value.
+   */
+  async reduce<X>(func: (prev: X, item: T) => X, initial: X): Promise<X> {
+    let result = initial;
+
+    for await (const item of this) {
+      result = func(result, item);
+    }
+
+    return result;
+  }
+
+  /**
+   * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Symbol/asyncIterator
+   */
+  next(): Promise<IteratorResult<T>> {
+    return this.#inner.next();
+  }
+
+  /**
+   * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Symbol/asyncIterator
+   */
+  [Symbol.asyncIterator](): AsyncIterator<T, any, undefined> {
+    return this;
+  }
+}
